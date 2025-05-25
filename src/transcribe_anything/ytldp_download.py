7dde68da1f6e432f270ca90c0773655935cf10e5
mdg-qc <99652049+mdg-qc@users.noreply.github.com>
2025-05-25 22:40:06 +0200

    Update ytldp_download.py

--- src/transcribe_anything/ytldp_download.py
+++ src/transcribe_anything/ytldp_download.py
@@ -15,7 +15,7 @@ def ytdlp_download(url: str, outdir: str) -> str:
     for file in os.listdir(outdir):
         os.remove(os.path.join(outdir, file))
     # cmd = f'yt-dlp --no-check-certificate {url} -o "out.%(ext)s"'
-    cmd = f'yt-dlp -x f "bestaudio" --audio-quality 0 --no-check-certificate {url} -o "out.%(ext)s"'
+    cmd = f'yt-dlp -x --audio-quality 0 --no-check-certificate {url} -o "out.%(ext)s"'
     
     print(f"Running:\n  {cmd}")
     subprocess.run(
